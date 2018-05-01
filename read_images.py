bb423b48ccdc60866783f68494fbe1e38fba14fd
Siyu <astro6974@gmail.com>
2018-05-01 17:37:53 +0200

    feature extraction with hog

--- read_images.py
+++ read_images.py
@@ -7,16 +7,18 @@ Created on Fri Apr 27 22:45:47 2018
 """
 
 
-from skimage import io
 import glob
 import numpy as np
 from sklearn.utils import shuffle
 from skimage.transform import resize
+from skimage import io, util, color
 
 
 train_image_list = []
 train_positive_image_list = []
 train_negative_image_list = []
+
+SAMPLE_SIZE = (256,128)
 
 for filename in sorted(glob.glob('projetpers/train/*.jpg')): #assuming gif
     im=io.imread(filename)
@@ -27,37 +29,35 @@ with open('projetpers/label.txt') as f:
     
 
 for i in range(0, len(lines)):
-    tmp_img = train_image_list[i]
+    tmp_img = color.rgb2gray(train_image_list[i])
     line = lines[i].split(" ")
     x = int(line[1])
     y = int(line[2])
     width = int(line[3])
     height = int(line[4])
     cropped_img = tmp_img[y:y+height, x:x+width]
-    resized_img = resize(cropped_img, (256, 256))
-    train_positive_image_list.append(resized_img.reshape(-1))
+    resized_img = resize(cropped_img, SAMPLE_SIZE)
+    train_positive_image_list.append(resized_img)
 
 # create 3 negative images for each raw_image
- 
-    max_height, max_width, deep = tmp_img.shape
-    
+#     
     
     cropped_img = tmp_img[y:, int((2*x+width)/2):]
-    resized_img = resize(cropped_img, (256, 256))
-    train_negative_image_list.append(resized_img.reshape(-1))
+    resized_img = resize(cropped_img, SAMPLE_SIZE)
+    train_negative_image_list.append(resized_img)
     
     
     cropped_img = tmp_img[0:x, 0:y]
-    resized_img = resize(cropped_img, (256, 256))
-    train_negative_image_list.append(resized_img.reshape(-1))
+    resized_img = resize(cropped_img, SAMPLE_SIZE)
+    train_negative_image_list.append(resized_img)
    
     cropped_img = tmp_img[y+height:, x+width:]
-    resized_img = resize(cropped_img, (256, 256))
-    train_negative_image_list.append(resized_img.reshape(-1))
+    resized_img = resize(cropped_img, SAMPLE_SIZE)
+    train_negative_image_list.append(resized_img)
 
 
-#    io.imshow(cropped_img)
-#   plt.show()
+#io.imshow(cropped_img)
+#plt.show()
 y_positive = np.ones(len(train_positive_image_list))
 y_negative = np.zeros(len(train_negative_image_list))
 train_positive_image_list = np.array(train_positive_image_list)
@@ -76,7 +76,6 @@ for name in dir():
     if not name.startswith('_') and name not in internal_vars:
         if name not in vars_to_keep:
             del globals()[name]
-            print(name)
 del vars_to_keep
 del internal_vars
 del name
